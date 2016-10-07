42c3a5d59f6c6490a9d2ae10dfc4d5223e812b61
advence-liz <1012515183@qq.com>
2016-10-07 17:06:48 +0800

    修改结构添加build逻辑

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require("gulp"), //本地安装gulp所用到的地方
     clean = require("gulp-clean"),
     less = require("gulp-less"),
     minifycss = require("gulp-minify-css"),
+    webpack = require('webpack-stream'),
     rename = require("gulp-rename"),
     babel = require("gulp-babel");
 
@@ -46,5 +47,22 @@ gulp.task('copy:fonts',["clean"],function(){
             gulp.dest('dist/font/')
         )
 })
+gulp.task('complie:less',['clean'],function(){
+    return gulp.src(['build/default/pack.less'])
+          .pipe(less())
+          .pipe(rename('widget.css'))
+          .pipe(gulp.dest('dist/css/'))
+})
 
-gulp.task("default", ["copy:js", "copy:css","copy:fonts"]);
+gulp.task('complie:js',['clean'],function(){
+        return gulp.src(['build/default/pack.js'])
+            .pipe(webpack({
+                output: {
+                    filename:'widget.js'
+                }
+            }))
+            .pipe(gulp.dest('dist/js/'))
+          
+})
+
+gulp.task("default", ["copy:js", "copy:css","copy:fonts","complie:less","complie:js"]);
