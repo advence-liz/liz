98c218395865b8c3a9abd1e74eb0fd7ea85280d7
advence-liz <1012515183@qq.com>
2016-11-04 17:16:13 +0800

    添加二级节点

--- src/js/tab.js
+++ src/js/tab.js
@@ -13,6 +13,12 @@
 							<li role="presentation" class="nav-item active"><a  class="nav-anchor" href="#">Home</a></li>
 							<li role="presentation" class="nav-item"><a  class="nav-anchor" href="#">Profile</a></li>
 							<li role="presentation" class="nav-item"><a   class="nav-anchor" href="#">Messages</a></li>
+                            <li role="presentation">
+                              <ul class="nav nav-tabs" role="tablist">
+							    <li role="presentation" class="nav-item"><a  class="nav-anchor" href="#">Home</a></li>
+							    <li role="presentation" class="nav-item"><a  class="nav-anchor" href="#">Profile</a></li>
+                              </ul>
+                            </li>
 						</ul>
                        
 					</aui-tab>
@@ -34,10 +40,16 @@ void function ($, window) {
         this.template = template || '<li role="presentation" class="nav-item" href="target_href"><a class="nav-anchor">dispaly_title</a></li>';
         this.target_href = target_href;
         this.dispaly_title = dispaly_title;
+        this.childList = new ArrayList;
         void function getTemplate() {
             this.template = this.template.replace(/target_href/, target_href).replace(/dispaly_title/, dispaly_title);
         }.apply(this);
     }
+    TabsItem.ul_front = '<ul class="nav nav-tabs" role="tablist">';
+    // TabsItem.ul_end='</ul>';
+    TabsItem.li_front = ' <li role="presentation">';
+    //  TabsItem.li_end='</li>';
+
     /**
      * @class ArrayList
      * @method unshift
@@ -47,8 +59,9 @@ void function ($, window) {
      * @method next
      */
     function ArrayList() {
-        var data_store = new Array;//private
-        cur_pos = 0;
+        var data_store = new Array,
+            cur_pos=0;
+         
         this.length = 0;
         this.push = function (arg) {
             data_store.push(arg);
@@ -82,11 +95,13 @@ void function ($, window) {
         this.remove = function (index, length) {
             var length = length || 1;
             data_store.splice(index, length);
+            this.length -= length;
             return this;
         };
         this.clear = function () {
             this.data_store.length = 0;
             this.length = 0;
+            return this;
         };
         /**
          * 获取 ArrayList 的下一个值
@@ -99,7 +114,15 @@ void function ($, window) {
             } else {
                 return !!(cur_pos = 0);
             }
+            //    if (this.cur_pos < this.length) {
+            //     return data_store[this.cur_pos++];
+            // } else {
+            //     return !!(this.cur_pos = 0);
+            // }
         };
+        this.positon = function () {
+            return this.cur_pos;
+        }
         this.each = function (func) {
             var index, item;
             for (index = this.length; i--;) {
@@ -112,7 +135,7 @@ void function ($, window) {
     }
     $.widget("aui.nav", {
         widgetEventPrefix: 'nav:',
-        $nav_ele:null,
+        $nav_ele: null,
         $navs_content: new $(),
         $nav_items: null,
         $cur_item: null,
@@ -128,10 +151,10 @@ void function ($, window) {
         _create: function () {
 
             this.element.append(this._createContent());
-            this.$nav_ele=this.element.find('.nav');
+            this.$nav_ele = this.element.find('.nav');
             this.$nav_items = this.element.find('.nav-item');
             this.$cur_item = new $(this.$nav_items[this.options.selectedIndex]);
-            this.$cur_content= new $(this.$navs_content[this.options.selectedIndex]);
+            this.$cur_content = new $(this.$navs_content[this.options.selectedIndex]);
             this._on({
                 "click .nav-item": function (event) {
                     this.$cur_item = $(event.currentTarget);
@@ -139,54 +162,83 @@ void function ($, window) {
                     this._refresh();
                 },
                 /**
-                 * ele.trigger('nav:invoke',{type:'remove',index:1});
+                 * ele.trigger('nav:invoke',{method:'remove',args:[]});
                  * @interface
                  * @param {Event}
                  * @param {planObject}
                  */
-                "nav:invoke":function(event,planObject){
-                    var method=planObject.method,
-                        args=planObject.args;
-                    switch (method){
-                        case 'remove':
-                        case 'insert':
-                        case 'push'  :
-                        case 'unshift':
-                        {
-                           this.options.tabList[method].apply(this,args);
-                           this._resetNav();
-                           break;
+                "nav:invoke": function (event, planObject) {
+                    var method = planObject.method,//{string}
+                        args = planObject.args;//{Array}
+                    try {
+                        switch (method) {
+                            case 'remove':
+                            case 'insert':
+                            case 'push':
+                            case 'unshift':
+                                {
+                                    this.options.tabList[method].apply(this, args);
+                                    this._resetNav();
+                                    break;
+                                }
                         }
-                    }
+                    } catch (error) {
+                        console.log(error);
+                    };
+
                 }
             });
             this._refresh();
 
         },
-        _setOption:function(key,value){
-           $.Widget.prototype._setOption.apply(this, arguments);
-           switch (key){
-               case 'tabList':{
+        _setOption: function (key, value) {
+            $.Widget.prototype._setOption.apply(this, arguments);
+            switch (key) {
+                case 'tabList': {
 
-               }
-           }
+                }
+            }
         },
         _createContent: function () {
             var template_arr = new Array,
                 template_str,//{string}
-                tab_list = this.options.tabList,//{ArrayList}
-                tmp_item,//{TabsItem}
+                tabList = this.options.tabList,//{ArrayList}
+                tmp,//{TabsItem}
                 class_str;//{string}
-            while (tmp_item = tab_list.next()) {
-                template_arr.push(tmp_item.template);
-                this.$navs_content = this.$navs_content.add(tmp_item.target_href);
+                
+            while (tmp = tabList.next()) {
+                if (tmp.childList.length) {
+                    template_arr.push(TabsItem.li_front);
+                    template_arr.push(TabsItem.ul_front);
+                    /**
+                     * 遍历二级节点
+                     * cur_item 二级节点中的当前li
+                     * chidList 二级节点集合 
+                     * 
+                     */
+                   var cur_item,childList=tmp.childList;
+                
+                    while ( cur_item= childList.next() ){
+                        template_arr.push(cur_item.template);
+                        this.$navs_content = this.$navs_content.add(cur_item.target_href);
+                    }
+                    template_arr.push('</ul>');
+                    template_arr.push('</li>');
+
+                } else {
+                    template_arr.push(tmp.template);
+                    this.$navs_content = this.$navs_content.add(tmp.target_href);
+                }
+
+
+
             }
             /**
-             * 此处可以加判断 设置nav 的tab 跟theme
+             * 此处可以加判断 设置nav 的tab 跟theme  DBZQ
              *  */
             //var reg= /<[^>]*class\s*=\s*"(.*)"\s.*/; 正则第一捕获为 class 属性的值
             class_str = 'nav' + ' ' + this.options.type + ' ' + this.options.theme;
-            template_arr.unshift('<ul class="nav" role="tablist">'.replace(/nav/, class_str));
+            template_arr.unshift(TabsItem.ul_front.replace(/nav/, class_str));
             template_arr.push('</ul>');
             template_str = template_arr.join(' ');
             return template_str;
@@ -194,20 +246,20 @@ void function ($, window) {
 
         },
         _refresh: function () {
-            this.options.selectedIndex=this.$cur_item.index();//此处只对click事件后调用的_refresh() 有意义
+            this.options.selectedIndex = this.$cur_item.index();//此处只对click事件后调用的_refresh() 有意义
             this.$nav_items.removeClass('active');
             this.$cur_item.addClass('active');
             this.$navs_content.removeClass("show active");
             this.$cur_content.addClass("show active");
         },
-        _resetNav:function(){
+        _resetNav: function () {
             this.$nav_ele.replaceWith(this._createContent());
-            this.$nav_ele=this.element.find('.nav');
+            this.$nav_ele = this.element.find('.nav');
             this.$nav_items = this.element.find('.nav-item');
             this.$cur_item = new $(this.$nav_items[this.options.selectedIndex]);
-            this.$cur_content= new $(this.$navs_content[this.options.selectedIndex]);
+            this.$cur_content = new $(this.$navs_content[this.options.selectedIndex]);
             this._refresh();
-            
+
         }
     })
 
